acba91bcdd4d8cfb18afaedaf0a81894c708d882
plussunilsingh <plussunilsingh@gmail.com>
2021-06-17 00:52:41 +0530

    jquery inculde in home page

---
+++ resources/js/custom.js
@@ -0,0 +1,10 @@
+/*
+Created by Alex Devero
+More information on 
+http://alexdevero.com
+
+To learn about HTML5, CSS3 and Javascript visit http://alexsblog.org
+*/
+$(document).ready(function(){
+    console.log("welcome home...");
+});
